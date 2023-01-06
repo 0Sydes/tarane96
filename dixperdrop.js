920e3cc756b72b04006932bef8aeb576ba1ef4ce
Sydes <46070813+0Sydes@users.noreply.github.com>
2023-01-06 17:50:33 +0100

    Create dixperdrop.js

---
+++ dixperdrop.js
@@ -0,0 +1,3 @@
+if (Number.isInteger(nombre)) return "/announceblue HolidayPresent " + Number + " DROP DIXPER ! Récupérez pour interagir avec le stream! " + Lien;
+else if (Link.substring(0, 32) == "https://dixper.gg/tarane96/drop/") return "/announceblue HolidayPresent DROP DIXPER ! Récupérez pour interagir avec le stream! " + Lien;
+else return "Le lien n'a pas été reconnu, veuillez réessayer avec un lien de drop dixper.";
