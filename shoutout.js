6aff1533f73d2b6436010ffa0c04b06d0d8ab51d
Sydes <46070813+0Sydes@users.noreply.github.com>
2023-01-06 16:16:14 +0100

    Update shoutout.js

--- shoutout.js
+++ shoutout.js
@@ -1,3 +1,3 @@
 if (us == "Tarane96") "Merci pour le raid Tarane96 ! Streameuse multi-gaming, vous trouverez sur sa chaîne du WoW, du Phasmophobia, du Ghost Exorcism Inc, et + encore ! https://www.twitch.tv/tarane96";
-else if (us == "0Sydes") "Test de mise à jour de commande...";
+else if (us == "0Sydes") "Test de mise à jour de commande n*2...";
 else "L'utilisateur n'a pas été trouvé ! :c";
