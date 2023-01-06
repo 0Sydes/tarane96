19b5c04d8d03a70e86a2e64c84a4f0d9510f1d02
Sydes <46070813+0Sydes@users.noreply.github.com>
2023-01-06 16:04:09 +0100

    Update shoutout.js

--- shoutout.js
+++ shoutout.js
@@ -1,2 +1,11 @@
-if (us == "Tarane96") {"Merci pour le raid Tarane96 ! Streameuse multi-gaming, vous trouverez sur sa chaîne du WoW, du Phasmophobia, du Ghost Exorcism Inc, et + encore ! https://www.twitch.tv/tarane96"}
-else {"L'utilisateur n'a pas été trouvé ! :c"}
+
+function shoutout(us) {
+  if (us == "Tarane96") {
+    return "Merci pour le raid Tarane96 ! Streameuse multi-gaming, vous trouverez sur sa chaîne du WoW, du Phasmophobia, du Ghost Exorcism Inc, et + encore ! https://www.twitch.tv/tarane96";
+  }
+  else {
+    return "L'utilisateur n'a pas été trouvé ! :c";
+  }
+}
+
+shoutout(us);
