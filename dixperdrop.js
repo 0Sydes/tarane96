7848673418e3420d280ad022a2787d66f90ee8f7
Sydes <46070813+0Sydes@users.noreply.github.com>
2023-01-06 18:44:19 +0100

    Update dixperdrop.js

--- dixperdrop.js
+++ dixperdrop.js
@@ -1,3 +1,3 @@
-if (Nombre != "" && Link.substring(0, 32) == "https://dixper.gg/tarane96/drop/") "/announceblue HolidayPresent " + Nombre + " DROP DIXPER ! Récupérez pour interagir avec le stream! " + Lien;
+if (Nombre != "" && Lien.substring(0, 32) == "https://dixper.gg/tarane96/drop/") "/announceblue HolidayPresent " + Nombre + " DROP DIXPER ! Récupérez pour interagir avec le stream! " + Lien;
 else if (Link.substring(0, 32) == "https://dixper.gg/tarane96/drop/") "/announceblue HolidayPresent DROP DIXPER ! Récupérez pour interagir avec le stream! " + Lien;
 else "Lien invalide!";
