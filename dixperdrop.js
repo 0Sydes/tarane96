44d9079720de3f552c54ae797757d93584974235
Sydes <46070813+0Sydes@users.noreply.github.com>
2023-01-06 18:16:54 +0100

    Update dixperdrop.js

--- dixperdrop.js
+++ dixperdrop.js
@@ -1,7 +1,7 @@
-if (Nombre != "" && Link.substring(0, 32) == "https://dixper.gg/tarane96/drop/") {
+if (Nombre != "" && Link.substring(0, 32) == "https://dixper.gg/tarane96/drop/")
   var msg = "/announceblue HolidayPresent " + Nombre + " DROP DIXPER ! Récupérez pour interagir avec le stream! " + Lien;
-  return msg;}
-else if (Link.substring(0, 32) == "https://dixper.gg/tarane96/drop/") {
+  return msg;
+else if (Link.substring(0, 32) == "https://dixper.gg/tarane96/drop/")
   var msg = "/announceblue HolidayPresent DROP DIXPER ! Récupérez pour interagir avec le stream! " + Lien;
-  return msg;}
+  return msg;
 else return "Lien invalide!";
