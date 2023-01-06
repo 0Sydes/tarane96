6606da918108ee3412dff407be3d353da78b688b
Sydes <46070813+0Sydes@users.noreply.github.com>
2023-01-06 15:57:50 +0100

    Update and rename shoutout.txt to shoutout.js

--- shoutout.js
+++ shoutout.js
@@ -1,1 +1,2 @@
 if (us == "Tarane96") {"Merci pour le raid Tarane96 ! Streameuse multi-gaming, vous trouverez sur sa chaîne du WoW, du Phasmophobia, du Ghost Exorcism Inc, et + encore ! https://www.twitch.tv/tarane96"}
+else {"L'utilisateur n'a pas été trouvé ! :c"}
